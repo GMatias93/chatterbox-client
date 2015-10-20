641536e45c63ca82f2b0c66029b17fdf492cff26
GMatias93 <gabriel.2m.deleon@gmail.com>
2015-10-19 21:09:37 -0700

    began interaction with the server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,25 +1,27 @@
 // YOUR CODE HERE:
 
-var app = {};
+window.app = {};
 
 app.init = function(){
+  app.server = 'https://api.parse.com/1/classes/chatterbox';
+  app.fetch();
+
   $(document).ready(function(){
     $(".username").on("click", app.addFriend());
   });
 
-  $(document).ready(function(){
+ $(document).ready(function(){
     $(".button").on("click", app.handleSubmit());
   })
+
+ 
 };
 
-var Message = function(username, text, roomname){
-  var message = {
-  "username": username,
-  "text": text,
-  "roomname": roomname
-  }
-  return message;
-};
+// var message = {
+// "username": username,
+// "text": text,
+// "roomname": roomname
+// }
 
 app.send = function(message){
   $.ajax({
@@ -38,14 +40,20 @@ app.send = function(message){
   });
 };
 
+
 app.fetch = function(){
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: undefined,
+    url: app.server,
     type: 'GET',
-    data: JSON.stringify(Message),
+    // data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
+      for(var i = 0; i < data.length; i++){
+        for(var key in data[i])
+        $(".chat").append(data[i][key]);
+      }
+      console.log(data);
       console.log('chatterbox: Message sent');
     },
     error: function (data) {
@@ -62,11 +70,12 @@ app.clearMessages = function(){
   }
 };
 
-app.addMessage = function(message){
+app.addMessage = function(message, username){
   var node = document.createElement("div");
-  var messageNode = document.createTextNode(Message);
+  var messageNode = document.createTextNode(message);
   node.appendChild(messageNode);
   document.getElementById("chats").appendChild(node);
+  app.send(message);
 };
 
 app.addRoom = function(roomName){
@@ -77,15 +86,23 @@ app.addRoom = function(roomName){
 };
 
 app.handleSubmit = function(){
-
-}
+  $(document).ready(function(){
+    $(".button").on("click", function(){
+    event.preventDefault();
+    var userMessage = $("#message").val()
+    app.addMessage(userMessage);
+    });
+  });
+};
 
 app.addFriend = function(){
 
 };
 
 
-
+$(document).ready(function(){
+  app.init();
+})
 
 
 
